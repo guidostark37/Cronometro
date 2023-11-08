4116d5fa19d918afb1e935b430cf30bbe98867a8
guidostark37 <bguido10@gmail.com>
2023-11-08 13:37:01 -0500

    sonidos

--- app/src/main/java/com/example/cronometro/MainActivity.kt
+++ app/src/main/java/com/example/cronometro/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cronometro
 
+import android.annotation.SuppressLint
 import android.graphics.Color
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -9,6 +11,8 @@ import com.example.cronometro.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var timer: CountDownTimer
+    lateinit var  mp:MediaPlayer
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,30 +20,44 @@ class MainActivity : AppCompatActivity() {
         var count =1
         var second:Long = 1000 * 61
         var minute =0
+        var hora = 0
+        mp = MediaPlayer.create(this,R.raw.sonido)
+
+
         timer = object : CountDownTimer(second,1000) {
             override fun onTick(millisUntilFinished: Long) {
                 binding.imageView.setImageDrawable(getDrawable(R.drawable.minutes))
                 binding.txttime.setTextColor(getColor(R.color.orange))
+
+
                 if (count == 60){
-                    minute +=1
+                    minute ++
                     count = 0
+                    if (minute ==60){
+                        hora ++
+                        minute = 0
+                    }
                 }
-                binding.txttime.text ="00:0${minute}:${count++}"
+                binding.txttime.text ="${hora}:${minute}:${count++}"
             }
             override fun onFinish() {
                 this.start()
             }
         }
         binding.btnstart.setOnClickListener {
-              timer.start()
+
+            mp.start()
+            timer.start()
+
+
+
         }
-        binding.btncancelar.setOnClickListener {
-            timer.cancel()
-        }
+
         binding.btnterminar.setOnClickListener {
 
             binding.imageView.setImageDrawable(getDrawable(R.drawable.minute))
             timer.cancel()
+            mp.pause()
             count = 0
             minute = 0
             binding.txttime.text ="00:0${minute}:${count++}"
